55ee617c535922a49fe1d515b24b566909c382b6
dimnl <d.c.m.hoogeveen@gmail.com>
2019-06-03 12:11:47 +0200

    Added comments

--- FP-Project/PComb.hs
+++ FP-Project/PComb.hs
@@ -15,11 +15,13 @@ data Stream = Stream [Char]
 ------------------------------------------------------------------
 --  D-3: FP1.1
 ------------------------------------------------------------------
+-- Parser that receive Stream and results in type a with remaining Stream
 newtype Parser a = P { runParser :: Stream -> [(a, Stream)]}
 
 ------------------------------------------------------------------
 --  D-3: FP1.2
 ------------------------------------------------------------------
+-- Functor instance that maps function over first value of tuple Parser
 instance Functor Parser where
   fmap f (P g) = P $ fmap (\(x,y)-> (f x, y)) . g
 
@@ -38,15 +40,34 @@ satisfy cond = P p
 char :: Char -> Parser Char
 char c = satisfy (==c)
 
+-- Simple tests
+parseOne :: Parser Char
+parseOne = char '1'
+
+parseOneInt :: Parser Int -- To check Functor
+parseOneInt = fmap digitToInt parseOne
+
+parseOneFail    = runParser parseOne (Stream "2a2")
+parseOneSatisfy = runParser parseOne (Stream "1a21")
+
+parseOneIntFail    = runParser parseOneInt (Stream "2a2")
+parseOneIntSatisfy = runParser parseOneInt (Stream "1a21")
+
 ------------------------------------------------------------------
 --  D-3: FP1.4
 ------------------------------------------------------------------
+-- Consumes no input and fails
 failure :: Parser a
 failure = P (\x -> [])
+
+-- Simple tests
+parseFailure1    = runParser failure (Stream "2a2")
+parseFailure2    = runParser failure (Stream "  a1a21")
 
 ------------------------------------------------------------------
 -- D-3: FP1.5
 ------------------------------------------------------------------
+-- Applicative instance for sequence combinator
 instance Applicative Parser where
   pure f = P (\x -> [(f, x)])
   (P f) <*> (P g) = P (\x -> [(r1 r2, x2) | (r1, x1) <- f x, (r2, x2) <- g x1])
@@ -54,6 +75,7 @@ instance Applicative Parser where
 ------------------------------------------------------------------
 -- D-3: FP1.6
 ------------------------------------------------------------------
+-- Alternative instance that tries as few alternatives as possible
 instance Alternative Parser where
   empty = failure
   (P f) <|> (P g) = P $ \x -> case f x of [] -> g x
@@ -62,6 +84,7 @@ instance Alternative Parser where
 ------------------------------------------------------------------
 -- D-3: FP1.7
 ------------------------------------------------------------------
+-- Monoid instance that tries all alternatives
 instance Monoid (Parser a) where
   mempty = empty
   mappend (P f) (P g)  = P (\x -> f x ++ g x)
@@ -69,18 +92,10 @@ instance Monoid (Parser a) where
 ------------------------------------------------------------------
 -- D-3: FP1.8
 ------------------------------------------------------------------
--- instance (Monoid a) => Monoid (Parser a) where
---   mempty = empty
---   mappend (P p1) (P p2)= p1 <*> p2
+-- Wrapper for Parser to define Monoid wrapper
+newtype ParseWrap a = PW { getParser :: Parser a}
 
-------------------------------------------------------------------
--- Some tests
-------------------------------------------------------------------
-parseOne :: Parser Char
-parseOne = char '1'
-
-parseOneInt :: Parser Int
-parseOneInt = fmap digitToInt parseOne
-
-parseOneIntFail    = runParser parseOneInt (Stream "2a2")
-parseOneIntSatisfy = runParser parseOneInt (Stream "1a21")
+-- Monoid wrapper that combines parsers sequentially
+instance (Monoid a) => Monoid (ParseWrap a) where
+  mempty = PW empty
+  mappend (PW f)  (PW g) = PW (mappend <$> f <*> g)
